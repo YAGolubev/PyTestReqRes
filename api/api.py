34852edf89d4d6375a0a3ea43fe131324ba0cd3e
YAGolubev <yagolubev@inbox.ru>
2023-11-07 23:23:00 +0300

    added api.py

---
+++ api/api.py
@@ -0,0 +1,24 @@
+import allure
+import requests
+
+
+class Api:
+    """Основной класс для работы с API"""
+    _HEADERS = {'Content-Type': 'application/json; charset=utf-8'}
+    _TIMEOUT = 10
+    base_url = {}
+
+    def __init__(self):
+        self.response = None
+
+    @allure.step("Отправить POST-запрос")
+    def post(self, url: str, endpoint: str, params: dict = None,
+             json_body: dict = None):
+        with allure.step(f"POST-запрос на url: {url}{endpoint}"
+                         f"\n тело запроса: \n {json_body}"):
+            self.response = requests.post(url=f"{url}{endpoint}",
+                                          headers=self._HEADERS,
+                                          params=params,
+                                          json=json_body,
+                                          timeout=self._TIMEOUT)
+        return self
