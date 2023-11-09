69c50a7d9b7cbadfa49dc6c3e2a9f41b067bfdb5
YAGolubev <yagolubev@inbox.ru>
2023-11-09 21:04:32 +0300

    added fixture

---
+++ fixture/reqres_api.py
@@ -0,0 +1,10 @@
+"""Фикстуры reqres api"""
+import pytest
+
+from api.reqres_api import ReqresApi
+
+
+@pytest.fixture(scope="function")
+def reqres_api() -> ReqresApi:
+    """Коннект к api reqres"""
+    return ReqresApi()
