fac7d9e768ede74759c3889dc531ae5a44081fbc
YAGolubev <yagolubev@inbox.ru>
2023-11-09 21:04:16 +0300

    added test

---
+++ test/test_delete_user.py
@@ -0,0 +1,15 @@
+"""Тест кейс для api reqres, delete user"""
+import allure
+import pytest
+
+from helper.load import load_data
+
+pytest_plugins = ["fixture.reqres_api"]
+pytestmark = [allure.parent_suite("reqres"),
+              allure.suite("delete_user")]
+
+
+@allure.title('Удаление пользователя')
+@pytest.mark.parametrize(('user_id', 'status'), load_data('delete_user_data'))
+def test_delete_user(reqres_api, user_id, status):
+    reqres_api.reqres_delete(user_id).status_code_should_be(status)
